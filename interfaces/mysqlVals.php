16d1ced54b71502dcc954040978fd5a73156b034
sssteveCA <steppuggio@gmail.com>
2022-04-30 15:17:02 +0200

    Added interfaces/MysqlVals.php

---
+++ interfaces/mysqlVals.php
@@ -0,0 +1,12 @@
+<?php
+
+//Mysql values
+namespace EcommerceTest\Interfaces;
+
+interface MySqlVals{
+    const HOST = 'localhost';
+    const USER = 'root';
+    const PASSWORD = '';
+    const DATABASE = 'stefano';
+}
+?>
