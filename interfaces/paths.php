85c535f74ab8fd0b9657189d73f8c58ac9a55418
sssteveCA <steppuggio@gmail.com>
2022-05-01 19:00:57 +0200

    Added interfaces/paths.php

---
+++ interfaces/paths.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace EcommerceTest\Interfaces;
+
+//absolute and relative paths constants
+interface Paths{
+
+    //absolute paths
+
+    //relative paths
+    const REL_ADVSEARCH_CSS = "css/avanzata.css";
+    const REL_ADVSEARCH_JS = "js/avanzata.js";
+    const REL_BOOTSTRAP_CSS = "bootstrap/css/bootstrap.min.css";
+    const REL_BOOTSTRAP_JS = "bootstrap/js/bootstrap.min.js";
+    const REL_BUY_CSS = "css/compra.css";
+    const REL_BUY_JS = "js/compra.js";
+    const REL_CART_CSS = "css/carrello.css";
+    const REL_CART_JS = "js/carrello.js";
+    const REL_CONFIRM_CSS = "css/conferma.css";
+    const REL_CONFIRM_JS = "js/conferma.js";
+    const REL_CONTACTS_CSS = "css/contatti.css";
+    const REL_CONTACTS_JS = "js/contatti.js";
+    const REL_CREATE_CSS = "css/crea.css";
+    const REL_CREATE_JS = "js/crea.js";
+    const REL_DIALOG_JS = "js/dialog/dialog.js";
+    const REL_EDIT_CSS = "css/edit.css";
+    const REL_EDIT_JS = "js/edit.js";
+    const REL_INFO_CSS = "css/info.css";
+    const REL_INFO_JS = "js/info.js";
+    const REL_INSERTIONS_CSS = "css/inserzioni.css";
+    const REL_INSERTIONS_JS = "js/inserzioni.js";
+    const REL_JQUERY = "js/jquery-3.5.1.min.js";
+    const REL_JQUERY_CSS = "jqueryUI/jquery-ui.min.css";
+    const REL_JQUERYTHEME_CSS = "jqueryUI/jquery-ui.theme.min.css";
+    const REL_JQUERYUI_JS = "jqueryUI/jquery-ui.min.js";
+    const REL_LOGINTO_CSS = "css/accedi/accedi.min.css";
+    const REL_LOGINTO_JS = "js/accedi.js";
+    const REL_LOGOUT_JS = "js/logout.js";
+    const REL_ORDERS_CSS = "css/ordini.css";
+    const REL_ORDERS_JS = "js/ordini.js";
+    const REL_PRODUCT_CSS = "css/prodotto.css";
+    const REL_PRODUCT_JS = "css/prodotto.css";
+    const REL_RECOVERY_CSS = "css/recupera/recupera.min.css";
+    const REL_RECOVERY_JS = "js/recupera.js";
+    const REL_RESET_CSS = "css/reset.css";
+    const REL_RESET_JS = "js/reset.js";
+    const REL_SEARCH_CSS = "css/ricerca.css";
+    const REL_SUBSCRIBE_CSS = "css/registrati/registrati.min.css";
+    const REL_SUBSCRIBE_JS = "js/registrati.js";
+    const REL_WELCOME_CSS = "css/benvenuto/benvenuto.min.css";
+    const REL_WELCOME_JS = "js/benvenuto.js";
+}
+?>
