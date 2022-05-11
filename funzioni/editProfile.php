36ddf70fc2dc7d215517416827c619eab123a11d
sssteveCA <steppuggio@gmail.com>
2022-05-11 09:06:24 +0200

    Updated editProfile.php
    Updated updatePassword() function

--- funzioni/editProfile.php
+++ funzioni/editProfile.php
@@ -95,19 +95,19 @@ else{
 //Update username field
 function updateUsername(){
     global $risposta,$utente;
-    $dati = array();
-    $dati['campo'] = 'username';
-    $dati['username'] = $utente->getUsername();
-    $dati['registrato'] = true;
+    $data = array();
+    $data['campo'] = 'username';
+    $data['username'] = $utente->getUsername();
+    $data['registrato'] = true;
     try{
-        $utente = new Utente($dati);
+        $utente = new Utente($data);
         $errno = $utente->getNumError();
         if($errno == 0 || $errno == Ue::INCORRECTLOGINDATA){
             $where = array();
             $where['username'] = $utente->getUsername();
             $risposta["msg"] = Msg::ERR_FORMINVALIDVALUE; 
-            $dati = array('username' => $_POST['username']);
-            $aggiorna = $utente->update($dati,$where);
+            $data = array('username' => $_POST['username']);
+            $aggiorna = $utente->update($data,$where);
             if($aggiorna){
                 $risposta["msg"] = Msg::USERUPDATED;
                 $_SESSION['welcome'] = '';
@@ -128,32 +128,48 @@ function updateUsername(){
         }
     }
     catch(Exception $e){
-
+        $risposta['msg'] = $e->getMessage();
     }
     
 }
 
 //Update password field
 function updatePassword(){
-    global $risposta,$utente,$where;
-    $passwordC = $utente->getPassword();
-    file_put_contents("log.txt","PasswordC => ".var_export($passwordC,true)."\r\n",FILE_APPEND);
-    file_put_contents("log.txt","POST => ".var_export($_POST,true)."\r\n",FILE_APPEND);
-    //se la password da sostituire è uguale a quella attuale
-    if(password_verify($_POST["oPwd"],$passwordC)){
-         //se la nuova password è uguale a quella confermata
-         if($_POST["nPwd"] == $_POST["confPwd"]){
-            $nuovo = array();
-            $nuovo['password'] = $_POST['nPwd'];
-            $aggiorna = $utente->update($nuovo,$where);
-            if($aggiorna){
-                $risposta["msg"] = Msg::PWDUPDATED;
-                $_SESSION['utente'] = serialize($utente);
-            }
-            else $risposta["msg"] = Msg::ERR_PWDNOTUPDATED;
-         }//if($_POST["nPwd"] == $_POST["confPwd"]){
-        else $risposta["msg"] = Msg::ERR_PWDCONFDIFFERENT;
-    }//if(password_verify($_POST["oPwd"],$passwordC)){
-    else $risposta["msg"] = Msg::ERR_PWDCURRENTWRONG;
+    global $risposta,$utente;
+    $data = array();
+    $data['campo'] = 'username';
+    $data['username'] = $utente->getUsername();
+    $data['registrato'] = true;
+    try{
+        $utente = new Utente($data);
+        $errno = $utente->getNumError();
+        if($errno == 0 || $errno == Ue::INCORRECTLOGINDATA){
+            $where = array();
+            $where['username'] = $utente->getUsername();
+            $risposta["msg"] = Msg::ERR_FORMINVALIDVALUE;  
+            $passwordC = $utente->getPassword();
+            file_put_contents("log.txt","PasswordC => ".var_export($passwordC,true)."\r\n",FILE_APPEND);
+            file_put_contents("log.txt","POST => ".var_export($_POST,true)."\r\n",FILE_APPEND);
+            //se la password da sostituire è uguale a quella attuale
+                if(password_verify($_POST["oPwd"],$passwordC)){
+                    //se la nuova password è uguale a quella confermata
+                    if($_POST["nPwd"] == $_POST["confPwd"]){
+                    $new = array();
+                    $new['password'] = $_POST['nPwd'];
+                    $aggiorna = $utente->update($new,$where);
+                    if($aggiorna){
+                        $risposta["msg"] = Msg::PWDUPDATED;
+                        $_SESSION['utente'] = serialize($utente);
+                    }
+                    else $risposta["msg"] = Msg::ERR_PWDNOTUPDATED;
+                    }//if($_POST["nPwd"] == $_POST["confPwd"]){
+                else $risposta["msg"] = Msg::ERR_PWDCONFDIFFERENT;
+            }//if(password_verify($_POST["oPwd"],$passwordC)){
+            else $risposta["msg"] = Msg::ERR_PWDCURRENTWRONG;
+        }//if($errno == 0 || $errno == Ue::INCORRECTLOGINDATA){
+    }
+    catch(Exception $e){
+        $risposta['msg'] = $e->getMessage();
+    } 
 }
 ?>
