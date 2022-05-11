6484fefa09eea04508981e3ec8e3545f5b088946
sssteveCA <steppuggio@gmail.com>
2022-05-11 08:50:43 +0200

    Updated editProfile.php
    Updated updateUsername() function

--- funzioni/editProfile.php
+++ funzioni/editProfile.php
@@ -42,26 +42,7 @@ if(isset($_SESSION['logged'],$_SESSION['utente'],$_SESSION['welcome']) && $_SESS
             && !preg_match($regex,$_POST["oPwd"]) 
             && !preg_match($regex,$_POST["nPwd"])
             && !preg_match($regex,$_POST["confPwd"]) && $_POST["pwd"] == "1"){
-                $passwordC = $utente->getPassword();
-                file_put_contents("log.txt","passwordC => ".var_export($passwordC,true)."\r\n",FILE_APPEND);
-                    //se la password da sostituire è uguale a quella attuale
-                    if(password_verify($_POST["oPwd"],$passwordC)){
-                            //se la nuova password è uguale a quella confermata
-                            if($_POST["nPwd"] == $_POST["confPwd"]){
-                                $nuovo = array();
-                                $nuovo['password'] = $_POST['nPwd'];
-                                $aggiorna = $utente->update($nuovo,$where); 
-                                //update('username',$_SESSION["user"],'password',password_hash($_POST["nPwd"],PASSWORD_DEFAULT));
-                                if($aggiorna){
-                                    $risposta["msg"] = Msg::PWDUPDATED;
-                                    $_SESSION['utente'] = serialize($utente);
-                                }
-                                else $risposta["msg"] = Msg::ERR_PWDNOTUPDATED;
-                            }//if($_POST["nPwd"] == $_POST["confPwd"]){
-                            else $risposta["msg"] = Msg::ERR_PWDCONFDIFFERENT;
-                        }//if(password_verify($_POST["oPwd"],$passwordC)){
-                        else $risposta["msg"] = Msg::ERR_PWDCURRENTWRONG;
-                
+               updatePassword();
             }//if(isset($_POST["oPwd"],$_POST["nPwd"],$_POST["confPwd"])&& !preg_match($regex,$_POST["oPwd"]) && !preg_match($regex,$_POST["nPwd"])&& !preg_match($regex,$_POST["confPwd"])){
             //se l'utente vuole modificare i suoi dati personali
             if(isset($_POST['nome'],$_POST["cognome"],$_POST["indirizzo"],$_POST["numero"],$_POST["citta"],$_POST["cap"],$_POST["pers"])){
@@ -113,22 +94,66 @@ else{
 
 //Update username field
 function updateUsername(){
-    global $risposta,$utente,$where;
-    $dati = array('username' => $_POST['username']);
-    $aggiorna = $utente->update($dati,$where);
-    if($aggiorna){
-        $risposta["msg"] = Msg::USERUPDATED;
-        $_SESSION['welcome'] = '';
-        if($utente->getSesso() == 'Maschio'){
-            $_SESSION['welcome'] = 'Benvenuto ';
+    global $risposta,$utente;
+    $dati = array();
+    $dati['campo'] = 'username';
+    $dati['username'] = $utente->getUsername();
+    $dati['registrato'] = true;
+    try{
+        $utente = new Utente($dati);
+        $errno = $utente->getNumError();
+        if($errno == 0 || $errno == Ue::INCORRECTLOGINDATA){
+            $where = array();
+            $where['username'] = $utente->getUsername();
+            $risposta["msg"] = Msg::ERR_FORMINVALIDVALUE; 
+            $dati = array('username' => $_POST['username']);
+            $aggiorna = $utente->update($dati,$where);
+            if($aggiorna){
+                $risposta["msg"] = Msg::USERUPDATED;
+                $_SESSION['welcome'] = '';
+                if($utente->getSesso() == 'Maschio'){
+                    $_SESSION['welcome'] = 'Benvenuto ';
+                }
+                else if($utente->getSesso() == 'Femmina'){
+                    $_SESSION['welcome'] = 'Benvenuta ';
+                }
+                $_SESSION['welcome'] .= $utente->getUsername();
+                $risposta["user"] = $utente->getUsername(); 
+                $_SESSION['utente'] = serialize($utente);
+            }
+            else $risposta["msg"] = Msg::ERR_USERNOTUPDATED;
+        }//if($errno == 0 || $errno == Ue::INCORRECTLOGINDATA){
+        else{
+            $risposta['msg'] = $utente->getStrError();
         }
-        else if($utente->getSesso() == 'Femmina'){
-            $_SESSION['welcome'] = 'Benvenuta ';
-        }
-        $_SESSION['welcome'] .= $utente->getUsername();
-        $risposta["user"] = $utente->getUsername(); 
-        $_SESSION['utente'] = serialize($utente);
     }
-    else $risposta["msg"] = Msg::ERR_USERNOTUPDATED;
+    catch(Exception $e){
+
+    }
+    
+}
+
+//Update password field
+function updatePassword(){
+    global $risposta,$utente,$where;
+    $passwordC = $utente->getPassword();
+    file_put_contents("log.txt","PasswordC => ".var_export($passwordC,true)."\r\n",FILE_APPEND);
+    file_put_contents("log.txt","POST => ".var_export($_POST,true)."\r\n",FILE_APPEND);
+    //se la password da sostituire è uguale a quella attuale
+    if(password_verify($_POST["oPwd"],$passwordC)){
+         //se la nuova password è uguale a quella confermata
+         if($_POST["nPwd"] == $_POST["confPwd"]){
+            $nuovo = array();
+            $nuovo['password'] = $_POST['nPwd'];
+            $aggiorna = $utente->update($nuovo,$where);
+            if($aggiorna){
+                $risposta["msg"] = Msg::PWDUPDATED;
+                $_SESSION['utente'] = serialize($utente);
+            }
+            else $risposta["msg"] = Msg::ERR_PWDNOTUPDATED;
+         }//if($_POST["nPwd"] == $_POST["confPwd"]){
+        else $risposta["msg"] = Msg::ERR_PWDCONFDIFFERENT;
+    }//if(password_verify($_POST["oPwd"],$passwordC)){
+    else $risposta["msg"] = Msg::ERR_PWDCURRENTWRONG;
 }
 ?>
