b3748bb8380724dcc1dedc5fc7b4f460537869e3
sssteveCA <steppuggio@gmail.com>
2022-06-14 23:12:29 +0200

    Updated insertion.controller.ts method for HTTP create insertion request

--- js/create/insertion.controller.js
+++ js/create/insertion.controller.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 //Create the Insertion in DB passing the Insertion object
 export default class InsertionController {
     constructor(insertion) {
@@ -9,12 +18,80 @@ export default class InsertionController {
     get errno() { return this._errno; }
     get error() {
         switch (this._errno) {
+            case InsertionController.ERR_INVALIDDATA:
+                this._error = InsertionController.ERR_MSG_INVALIDDATA;
+                break;
+            case InsertionController.ERR_NOINSERTIONOBJECT:
+                this._error = InsertionController.ERR_MSG_NOINSERTIONOBJECT;
+                break;
             default:
                 this._error = null;
                 break;
         }
         return this._error;
     }
+    createPromise() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield new Promise((resolve, reject) => {
+                const headers = {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json'
+                };
+                const request = fetch(InsertionController.INSERTION_URL, {
+                    method: 'POST',
+                    body: JSON.stringify(this._insertion),
+                    headers: headers
+                });
+                request.then(r => {
+                    resolve(r.text());
+                }).catch(err => {
+                    reject(InsertionController.ERR_MSG_INSERTIONEERROR);
+                });
+            });
+        });
+    }
+    //HTTP request for insert the insertion in MySql DB
+    createInsertion() {
+        this._errno = 0;
+        if (this._insertion != null) {
+            //Insertion object is istantiated
+            if (this.validateInsertion()) {
+                //All data are valid
+                this.createPromise().then(res => {
+                    console.log(res);
+                }).catch(err => {
+                    console.warn(err);
+                });
+            } //if(this.validateInsertion()){
+            else
+                this._errno = InsertionController.ERR_INVALIDDATA;
+        } //if(this._insertion != null){
+        else
+            this._errno = InsertionController.ERR_NOINSERTIONOBJECT;
+    }
+    //Check if Insertion model has required properties
+    validateInsertion() {
+        let ok = false;
+        let okName = (this._insertion.name != '');
+        let okImage = (this._insertion.image != null && this._insertion.image.size > 0);
+        let okType = (this._insertion.type != '');
+        let okPrice = (this._insertion.price > 0);
+        let okShipping = (this._insertion.shipping > 0);
+        let okCondition = (this._insertion.condition != '');
+        let okState = (this._insertion.state != '');
+        let okCity = (this._insertion.city != '');
+        let okDescription = (this._insertion.description != '');
+        if (okName && okImage && okType && okPrice && okShipping && okCondition && okState && okCity && okDescription) {
+            ok = true;
+        }
+        return ok;
+    }
 }
 //constants
 InsertionController.INSERTION_URL = 'funzioni/upload.php';
+//errors
+InsertionController.ERR_INVALIDDATA = 1; //Data from form are invalid
+InsertionController.ERR_NOINSERTIONOBJECT = 2; //Insertion object is null
+InsertionController.ERR_MSG_INVALIDDATA = "Uno o più valori inseriti non sono validi";
+InsertionController.ERR_MSG_NOINSERTIONOBJECT = "L'oggetto Insertion è uguale a null";
+InsertionController.ERR_MSG_INSERTIONEERROR = "Errore durante l' inserimento dell'inserzione. Riprovare più tardi e se il problema persiste contattare l'amministratore del sito";
--- js/create/insertion.controller.ts
+++ js/create/insertion.controller.ts
@@ -8,6 +8,13 @@ export default class InsertionController{
     private static INSERTION_URL = 'funzioni/upload.php';
 
     //errors
+    private static ERR_INVALIDDATA = 1; //Data from form are invalid
+    private static ERR_NOINSERTIONOBJECT = 2; //Insertion object is null
+
+    private static ERR_MSG_INVALIDDATA = "Uno o più valori inseriti non sono validi";
+    private static ERR_MSG_NOINSERTIONOBJECT = "L'oggetto Insertion è uguale a null";
+
+    private static ERR_MSG_INSERTIONEERROR = "Errore durante l' inserimento dell'inserzione. Riprovare più tardi e se il problema persiste contattare l'amministratore del sito";
 
     //properties
     private _insertion: Insertion;
@@ -18,14 +25,80 @@ export default class InsertionController{
         this._insertion = insertion;
     }
 
+
     get insertion(){return this._insertion;}
     get errno(){return this._errno;}
     get error(){
         switch(this._errno){
+            case InsertionController.ERR_INVALIDDATA:
+                this._error = InsertionController.ERR_MSG_INVALIDDATA;
+                break;
+            case InsertionController.ERR_NOINSERTIONOBJECT:
+                this._error = InsertionController.ERR_MSG_NOINSERTIONOBJECT;
+                break;
             default:
                 this._error = null;
                 break;
         }
         return this._error;
     }
+
+    private async createPromise(): Promise<any>{
+        return await new Promise((resolve,reject) => {
+            const headers = {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            };
+            const request = fetch(InsertionController.INSERTION_URL,{
+                method: 'POST',
+                body: JSON.stringify(this._insertion),
+                headers: headers
+            });
+            request.then(r => {
+                resolve(r.text());
+            }).catch(err => {
+                reject(InsertionController.ERR_MSG_INSERTIONEERROR);
+            });
+        });
+    }
+
+    //HTTP request for insert the insertion in MySql DB
+    private createInsertion(): void{
+        this._errno = 0;
+        if(this._insertion != null){
+            //Insertion object is istantiated
+            if(this.validateInsertion()){
+                //All data are valid
+                this.createPromise().then(res => {
+                    console.log(res);
+                }).catch(err => {
+                    console.warn(err);
+                });
+            }//if(this.validateInsertion()){
+            else
+                this._errno = InsertionController.ERR_INVALIDDATA;
+        }//if(this._insertion != null){
+        else
+            this._errno = InsertionController.ERR_NOINSERTIONOBJECT;
+    }
+
+    //Check if Insertion model has required properties
+    private validateInsertion(): boolean{
+        let ok = false;
+        let okName: boolean = (this._insertion.name != '');
+        let okImage: boolean = (this._insertion.image != null && this._insertion.image.size > 0);
+        let okType: boolean = (this._insertion.type != '');
+        let okPrice: boolean = (this._insertion.price > 0);
+        let okShipping: boolean = (this._insertion.shipping > 0);
+        let okCondition: boolean = (this._insertion.condition != '');
+        let okState: boolean = (this._insertion.state != '');
+        let okCity: boolean = (this._insertion.city != '');
+        let okDescription: boolean = (this._insertion.description != '');
+        if(okName && okImage && okType && okPrice && okShipping && okCondition && okState && okCity && okDescription){
+            ok = true;
+        }
+        return ok;
+    }
+
+
 }
