5f5259370144826e3b0b170faacc440d9fcbe646
sssteveCA <steppuggio@gmail.com>
2022-08-07 19:04:18 +0200

    Added js/orders/order.interface.ts

---
+++ js/orders/interfaces/order.interface.ts
@@ -0,0 +1,12 @@
+export default interface OrderInterface{
+    id: number; //Order id
+    idc: number; //Customer id
+    idp: number; //Product id
+    idv: number; //Seller id
+    date: string; //Order created date
+    quantity: number; //Number of products in order
+    total: number; //Total price
+    payed: boolean; //If order was payed
+    tnx_id: string|null; //Transaction id(null if order not payed yet)
+    cart: boolean; //If order was added in cart
+}
