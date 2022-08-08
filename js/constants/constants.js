283b564ceb2033c41f2ee17ec732560e6757b0a2
sssteveCA <steppuggio@gmail.com>
2022-08-08 20:07:46 +0200

    Unstaged js files compiled by typescript

--- js/constants/constants.js
+++
@@ -1,4 +0,0 @@
-export var Constants;
-(function (Constants) {
-    Constants.MSG_CONFIRM_ORDERDELETE = "Sei sicuro di voler cancellare definivamente quest'ordine?";
-})(Constants || (Constants = {}));
