241947cdc11aca28d0fdd19f1e2e90dcf69505ac
sssteveCA <steppuggio@gmail.com>
2022-08-08 12:26:25 +0200

    Added getOrder & getOrderPromise methods in js/orders/requests/getorder.ts

--- js/orders/requests/getorder.js
+++ js/orders/requests/getorder.js
@@ -1,8 +1,53 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 export default class GetOrder {
     constructor(data) {
+        this._errno = 0;
+        this._error = null;
         this._id_order = data.id_order;
         this._operation = data.operation;
     }
     get id_order() { return this._id_order; }
     get operation() { return this._operation; }
+    get errno() { return this._errno; }
+    get error() {
+        switch (this._errno) {
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
+    getOrder() {
+        return __awaiter(this, void 0, void 0, function* () {
+            let order = null;
+            return order;
+        });
+    }
+    getOrderPromise() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield new Promise((resolve, reject) => {
+                let body_params = `?idOrd=${this._id_order}&oper=${this._operation}`;
+                fetch(GetOrder.GETORDER_URL, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                    },
+                    body: body_params
+                }).then(res => {
+                    resolve(res.text());
+                }).catch(err => {
+                    reject(err);
+                });
+            });
+        });
+    }
 }
+GetOrder.GETORDER_URL = 'funzioni/orderMan.php';
--- js/orders/requests/getorder.ts
+++ js/orders/requests/getorder.ts
@@ -1,8 +1,14 @@
 import GetOrderInterface from "../interfaces/getorder.interface";
+import Order from "../models/order.model";
 
 export default class GetOrder{
     private _id_order: string; //Order to be obtained
     private _operation: string; //command to sent at backend to get the order
+    private _order: Order;
+    private _errno: number = 0;
+    private _error: string|null = null;
+
+    private static GETORDER_URL:string = 'funzioni/orderMan.php'
 
     constructor(data: GetOrderInterface){
         this._id_order = data.id_order;
@@ -11,4 +17,35 @@ export default class GetOrder{
 
     get id_order(){return this._id_order;}
     get operation(){return this._operation;}
+    get errno(){return this._errno;}
+    get error(){
+        switch(this._errno){
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
+
+    public async getOrder(): Promise<Order|null>{
+        let order: Order|null = null;
+        return order;
+    }
+
+    public async getOrderPromise(): Promise<string>{
+        return await new Promise<string>((resolve,reject)=>{
+            let body_params:string = `?idOrd=${this._id_order}&oper=${this._operation}`;
+            fetch(GetOrder.GETORDER_URL,{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                },
+                body: body_params
+            }).then(res => {
+                resolve(res.text());
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
 }
