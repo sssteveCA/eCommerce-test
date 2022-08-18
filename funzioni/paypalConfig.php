fbfe4691861eca698c49476f0230d5049829290e
sssteveCA <steppuggio@gmail.com>
2022-08-18 21:28:19 +0200

    Added EcommnerceTest\Config interface in funzioni/paypalConfig.php

--- funzioni/paypalConfig.php
+++ funzioni/paypalConfig.php
@@ -1,4 +1,6 @@
 <?php
+
+use EcommerceTest\Config;
 //Client Id of REST app
 define("CLIENT_ID", "AQ-FlbKtljViykaUcwcbrsuQRfCgeKShBGzknTdE9zwD9uOudjUEypHdjA72kDpRxIMoyO0i7bBRQajw");
 
@@ -13,7 +15,7 @@ $ajax =  (isset($_POST['ajax']) && $_POST['ajax'] == '1');
 //$emailBusiness = 'sb-svkih5058080@business.example.com';
 //pagina di Paypal a cui verranno inviati i dati del form
 $paypalPage = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
-$shopping_url = 'http://localhost/php/curl/accounts/ricerca.php?ricerca=';
+$shopping_url = Config::HOME_URL.'/ricerca.php?ricerca=';
 /*metodo di ritorno, ovvero come i dati verranno inviati da paypal alla pagina di ritorno scelta
 0 = metodo GET per tutte le transazioni
 1 = il metodo GET viene usato per il redirect del browser verso la pagina di ritornom senza inviare i dati della transazione
