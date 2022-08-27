4b73ec0dfb163a72e9af9c70097183d3ab506dea
Acer <Acer@DESKTOP-K09QF87>
2022-08-27 18:11:05 +0200

    Added properties,constructors & getters in getordersincart.ts

---
+++ js/cart/requests/getordersincart.ts
@@ -0,0 +1,38 @@
+import GetOrdersInCartInterface from "../interfaces/getordersincart.interface";
+import CartOrder from "../models/cartorder.model";
+
+export default class GetOrdersInCart{
+    private _operation: number; //command to sent at backend to get orders in backend
+    private _cart_orders: CartOrder[] = [];
+    private _length: number = 0; //Number of orders in cart retrieved
+    private _errno: number = 0;
+    private _error: string|null = null;
+
+    private static GETORDERSINCART_URL:string = 'funzioni/cartMan.php';
+
+    //Error numbers
+    public static ERR_FETCH:number = 1;
+
+    //Error messages
+    private static ERR_FETCH_MSG:string = "Errore durante la richiesta dei dati";
+
+    constructor(data: GetOrdersInCartInterface){
+        this._operation = data.operation;
+    }
+
+    get operation(){return this._operation;}
+    get cart_orders(){return this._cart_orders;}
+    get length(){return this._length;}
+    get errno(){return this._errno;}
+    get error(){
+        switch(this._errno){
+            case GetOrdersInCart.ERR_FETCH:
+                this._error = GetOrdersInCart.ERR_FETCH_MSG;
+                break;
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
+}
