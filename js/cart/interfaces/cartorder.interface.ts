72d17345847ab8aa530e68bfe259b6589699a328
Acer <Acer@DESKTOP-K09QF87>
2022-08-27 17:57:24 +0200

    Added js/cart/interfaces/cartorder.interface.ts

---
+++ js/cart/interfaces/cartorder.interface.ts
@@ -0,0 +1,12 @@
+
+export default interface CartOrderInterface{
+    ido: number; //Order id
+    idp: number; //Product id
+    idv: number; //Seller id of this product
+    image: string; //URL of product image
+    type: string; //Product category
+    product_price: number; //Single product price
+    quantity: number; //Number of products in this order
+    shipping: number; //Shipping cost of this order
+    total: number; //Total price of the order
+}
