f7ba127499b6362c9ac41bb1977065d3da6ec3a0
Acer <Acer@DESKTOP-K09QF87>
2022-09-16 09:33:32 +0200

    Added Bootstrap carousel in benvenuto.php

--- benvenuto.php
+++ benvenuto.php
@@ -37,6 +37,33 @@ if(isset($_SESSION['logged'],$_SESSION['utente'],$_SESSION['welcome']) && $_SESS
             </form>
             <p id="rAvanzata" class="flex-fill d-flex justify-content-center"><a href="avanzata.php">Ricerca avanzata</a></p>
         </div>
+        <div class="carousel-container">
+            <div id="homepage-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="3000">
+                        <img src="img/altre/homepage_carousel/ecommerce-background1.jpg" class="d-block" alt="ECommerce">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="3000">
+                        <img src="img/altre/homepage_carousel/ecommerce-background2.jpg" class="d-block" alt="ECommerce">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="3000">
+                        <img src="img/altre/homepage_carousel/ecommerce-background3.jpg" class="d-block" alt="ECommerce">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="3000">
+                        <img src="img/altre/homepage_carousel/ecommerce-background4.jpg" class="d-block" alt="ECommerce">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#homepage-carousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Precedente</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#homepage-carousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Successivo</span>
+                </button>
+            </div>
+        </div>
+        
     </body>
 </html>
 <?php
