8a2c6df9fd0f957a4e7e1a66724f9789410dfed1
Acer <Acer@DESKTOP-K09QF87>
2022-10-23 17:44:05 +0200

    Updated footer description in footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,23 @@
 function footer(){
     $html = <<<HTML
 <div class="footer">
-    <div class="d-flex justify-content-center">© 2022 - Stefano Puggioni</div>
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                ECommerce è un sito web che ti permette di effettuare acquisti di vari tipi di prodotti. E' sufficiente creare un account in maniera gratuita.
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                Questa sito web è sviluppato da <a href="https://github.com/sssteveCA/eCommerce-test">Stefano Puggioni</a>.
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                © 2022 - ECommerce
+            </div>
+        </div>
+    </div>
 </div>
 HTML;
     return $html;
