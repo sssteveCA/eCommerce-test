3611bf3e63fa705324e7fd09148fdbc287bcab89
Acer <Acer@DESKTOP-K09QF87>
2022-11-13 22:37:27 +0100

    Added sendRecoveryMail and sendRecoveryMailPromise methods to recovery.controller.ts

--- js/recovery/recovery.controller.ts
+++ js/recovery/recovery.controller.ts
@@ -30,4 +30,40 @@ export default class RecoveryController{
         return this._error;
     }
 
+    public async sendRecoveryMail(): Promise<object>{
+        this._errno = 0;
+        let response: object = {};
+        try{
+            await this.sendRecoveryMailPromise().then(res => {
+                console.log(res);
+                response = JSON.parse(res);
+            }).catch(err => {
+                throw err;
+            });
+        }catch(e){
+            this._errno = RecoveryController.ERR_REQUEST;
+            response = { msg: this.error };
+        }
+        return response;
+    }
+
+    private async sendRecoveryMailPromise(): Promise<string>{
+        return await new Promise<string>((resolve, reject) => {
+            let params: object = {
+                method: 'POST',
+                body: `ajax=1&email=${this._email}`,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            };
+            const response = fetch(RecoveryController.RECOVERY_URL,params);
+            response.then(r => {
+                resolve(r.text());
+            }).catch(err => {
+                console.warn(err);
+                reject(RecoveryController.ERR_REQUEST_MSG);
+            });
+        });
+    }
+
 }
