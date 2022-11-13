d9397d1c0c0bb0b7e751f8ed19c74300a76ac412
Acer <Acer@DESKTOP-K09QF87>
2022-11-13 23:25:33 +0100

    Added HTTP request via ResetController in reset.ts

--- js/reset/reset.ts
+++ js/reset/reset.ts
@@ -1,6 +1,27 @@
+import DialogMessage from "../dialog/dialogmessage";
+import DialogMessageInterface from "../dialog/dialogmessage.interface";
+import ResetController from "./reset.controller";
+import ResetInterface from "./reset.interface";
 
 $(()=>{
     $('#fRecupera').on('submit',(e)=>{
         e.preventDefault();
+        let data: ResetInterface = {
+            key: $('#chiave').val() as string,
+            newPassword: $('#nuova').val() as string,
+            confPassword: $('#confNuova').val() as string
+        };
+        let reset: ResetController = new ResetController(data);
+        reset.resetPassword().then(obj => {
+            let dmData: DialogMessageInterface = {
+                title: 'Reimpostazione password',
+                message: obj["msg"]
+            };
+            let dm: DialogMessage = new DialogMessage(dmData);
+            dm.btOk.on('click',()=>{
+                dm.dialog.dialog('destroy');
+                dm.dialog.remove();
+            });
+        });
     });
 });
