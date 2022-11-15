a8eeecbf1bb279754907fce75cb92a49dec1f5a2
Acer <Acer@DESKTOP-K09QF87>
2022-11-15 08:36:45 +0100

    Removed reset.js,recupera.js,ordini.js,logout.js,edit.js,carrello.js,contatti.js and crea.js from js folder

--- js/logout.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    //l'utente vuole disconnettersi
-    $('#logout a').on('click',function(e){
-        e.preventDefault();
-        DialogRedirect('Esci','Sei sicuro di voler abbandonare la sessione?','funzioni/logout.php');
-    });
-});
