36f642e64c1ba4f6c3ddfe6d5a9e80b3afd5e17e
Acer <Acer@DESKTOP-K09QF87>
2022-11-20 11:06:28 +0100

    Added Send product seller mail request to product.ts

--- js/product/product.ts
+++ js/product/product.ts
@@ -1,7 +1,27 @@
+import ProductMailInterface from "./interfaces/productmail.interface";
+import ProductMailController from "./requests/productmail.controller";
+import DialogMessageInterface from "../dialog/dialogmessage.interface";
+import DialogMessage from "../dialog/dialogmessage";
 
 $(()=>{
     $('#formMail').on('submit',(e)=>{
        e.preventDefault();
+       let pmData: ProductMailInterface = {
+        email: $('#emailTo').val() as string,
+        subject: $('#oggetto').val() as string,
+        message: $('#messaggio').val() as string
+       };
+       let pmc: ProductMailController = new ProductMailController(pmData);
+       pmc.sendMail().then(obj => {
+        let dmData: DialogMessageInterface = {
+            title: 'Mail al venditore', message: obj["msg"]
+        };
+        let dm: DialogMessage = new DialogMessage(dmData);
+        dm.btOk.on('click',()=>{
+            dm.dialog.dialog('destroy');
+            dm.dialog.remove();
+        });
+       });
     });
     $('#elimina').on('submit',(e)=>{
         e.preventDefault();
