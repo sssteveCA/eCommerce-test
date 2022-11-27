f41fd14bb29143d4a4e198c268c2eb48d7f695ee
Acer <Acer@DESKTOP-K09QF87>
2022-11-27 19:27:38 +0100

    Added setServerSettings method to emailmanager.php

--- objects/emailmanager.php
+++ objects/emailmanager.php
@@ -4,6 +4,7 @@ namespace EcommerceTest\Objects;
 
 use EcommerceTest\Traits\Error;
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
 class EmailManager extends PHPMailer{
 
@@ -11,6 +12,11 @@ class EmailManager extends PHPMailer{
 
     /**
      * Sender email
+     */
+    private string $fromEmail;
+
+    /**
+     * Receiver email
      */
     private string $toEmail;
 
@@ -29,6 +35,7 @@ class EmailManager extends PHPMailer{
         
     }
 
+    public function getFromEmail(){ return $this->fromEmail; }
     public function getToEmail(){ return $this->toEmail; }
     public function getSubject(){ return $this->subject; }
     public function getBody(){ return $this->body; }
@@ -37,10 +44,22 @@ class EmailManager extends PHPMailer{
      * Check if the needed values exist
      */
     private function checkExists(array $data): bool{
-        if(isset($data['to']))return false;
-        if(isset($data['subject']))return false;
-        if(isset($data['body']))return false;
+        if(!isset($data['from']))return false;
+        if(!isset($data['to']))return false;
+        if(!isset($data['subject']))return false;
+        if(!isset($data['body']))return false;
         return true;
+    }
+
+    private function setServerSettings(array $data){
+        $this->SMTPDebug = SMTP::DEBUG_OFF;
+        $this->isSMTP();
+        $this->Host = isset($data['MAIL_HOST']) ? $data['MAIL_HOST'] : $_ENV['MAIL_HOST'];
+        $this->SMTPAuth = true;
+        $this->Username = isset($data['MAIL_USERNAME']) ? $data['MAIL_USERNAME'] : $_ENV['MAIL_USERNAME'];
+        $this->Password = isset($data['MAIL_PASSWORD']) ? $data['MAIL_PASSWORD'] : $_ENV['MAIL_PASSWORD'];
+        $this->SMTPSecure = false;
+        $this->Port = isset($data['MAIL_PORT']) ? $data['MAIL_PORT'] : $_ENV['MAIL_PORT'];
     }
 }
 ?>
