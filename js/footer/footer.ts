b4de1dead0decaec9a617c0cf22f5947e99ff310
stepp <stepp@mail.com>
2022-12-18 17:12:50 +0100

    Updated footerEl position attribute condition in footerPosition function of footer.ts

--- js/footer/footer.ts
+++ js/footer/footer.ts
@@ -16,7 +16,11 @@ $(()=>{
 function footerPosition(footerEl: JQuery): void{
     let wHeight: number = $(window).height() as number;
     let bHeight: number = $('body').height() as number;
-    if(bHeight < wHeight){
+    let footerH: number = footerEl.height() as number;
+    /* console.log("wWeight => "+wHeight);
+    console.log("bHeight => "+bHeight);
+    console.log("footerH => "+footerH); */
+    if(bHeight < wHeight - footerH){
         footerEl.css({
             position: 'fixed', bottom: '0px'
         });
