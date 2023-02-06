6c824394bdf5a0d95e0f7a76a56670e6ee9956d2
stepp <stepp@mail.com>
2023-02-06 19:15:58 +0100

    Removed cookie_policy.php, privacy_policy.php, terms.php from .gitignore excluded files

---
+++ privacy_policy.php
@@ -0,0 +1,54 @@
+<?php
+
+use EcommerceTest\Objects\Privacy\PrivacyPolicy;
+use EcommerceTest\Interfaces\Paths as P;
+
+require_once("interfaces/paths.php");
+require_once("partials/privacy.php");
+require_once("partials/navbar.php");
+require_once("objects/privacy/privacypolicy.php");
+require_once("partials/footer.php");
+
+$bsCss = P::REL_BOOTSTRAP_CSS;
+$footerCss = P::REL_FOOTER_CSS;
+$popperJs = P::REL_POPPER_JS;
+$bsJs = P::REL_BOOTSTRAP_JS;
+$footerJs = P::REL_FOOTER_JS;
+$logoutJs = P::REL_LOGOUT_JS;
+
+$loggedCond = (isset($_SESSION['logged'],$_SESSION['utente'],$_SESSION['welcome']) && $_SESSION['welcome'] != '' && $_SESSION['logged'] === true);
+
+$html = <<<HTML
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Privacy Policy</title>
+        <meta charset="utf-8">
+		<link rel="stylesheet" href="{$bsCss}">
+		<link rel="stylesheet" href="{$footerCss}">
+		<script src="{$popperJs}"></script>
+        <script src="{$bsJs}"></script>
+        <script src="{$footerJs}"></script>
+HTML;
+
+if($loggedCond) $html .= <<<HTML
+<script type="module" src="{$logoutJs}"></script>
+HTML;
+
+$html .= cookieBanner();
+$html .= <<<HTML
+	</head>
+	<body>
+HTML;
+
+if($loggedCond) $html .= menu($_SESSION['welcome']);
+
+$html .= PrivacyPolicy::getDocument();
+$html .= footer();
+$html .= <<<HTML
+	</body>
+</html>
+HTML;
+
+echo $html;
+?>
