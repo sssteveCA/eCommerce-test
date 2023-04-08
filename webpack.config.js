a739d714132a4cbdea986710703d2db48e975544
stepp <stepp@mail.com>
2023-04-08 23:49:55 +0200

    Added .ts entry points to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
 
-const path = require('path')
+const {join, resolve} = require('path')
+
+const tsSrc = join(__dirname,'ts')
 
 module.exports = {
     entry: {
-        index: './ts/index.ts'
+        'advanced': join(tsSrc,'advanced/advanced.ts'),
+        'buy': join(tsSrc,'buy/buy.ts'),
+        'cart': join(tsSrc,'cart/cart.ts'),
+        'confirm': join(tsSrc,'confirm/confirm.ts'),
+        'constants': join(tsSrc,'constants/constants.ts'),
+        'contacts': join(tsSrc,'contacts/contacts.ts'),
+        'create': join(tsSrc,'create/insertion.ts'),
+        'edit': join(tsSrc,'edit/edit.ts'),
+        'footer': join(tsSrc,'footer/footer.ts'),
+        'info': join(tsSrc,'info/info.ts'),
+        'insertions': join(tsSrc,'insertions/insertions.ts'),
+        'log_into': join(tsSrc,'log_into/log_into.ts'),
+        'logout': join(tsSrc,'logout/logout.ts'),
+        'orders': join(tsSrc,'orders/orders.ts'),
+        'product': join(tsSrc,'product/product.ts'),
+        'recovery': join(tsSrc,'recovery/recovery.ts'),
+        'reset': join(tsSrc,'reset/reset.ts'),
+        'subscribe': join(tsSrc,'subscribe/subscribe.ts'),
+        'welcome': join(tsSrc,'welcome/welcome.ts'),
     },
     output: {
         path: path.resolve(__dirname,'js'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
         rules: [
