66f825ae585cf20312c7a06a90d11932ee51cf3c
stepp <stepp@mail.com>
2023-04-08 23:10:27 +0200

    Updated webpack.config.js, added typescript config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
 
+const path = require('path')
+
 module.exports = {
     entry: {
         index: './ts/index.ts'
     },
     output: {
-        path: './js',
+        path: path.resolve(__dirname,'js'),
         filename: 'bundle.js'
     },
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
     },
     plugins: [],
-    devServer: {}
+    resolve: {
+      extension: ['.tsx','.ts','.js']  
+    },
+    devServer: {
+        port: 5000,
+        open: true,
+        static: path.resolve(__dirname)
+    },
+    mode: 'development'
 }
