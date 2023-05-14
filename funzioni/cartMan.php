7035bc76ee9d1a8ceb0749c4f5f741845f355a26
stepp <stepp@mail.com>
2023-05-14 23:09:22 +0200

    Added files property to composer.json

--- funzioni/cartMan.php
+++ funzioni/cartMan.php
@@ -1,30 +1,7 @@
 <?php
 session_start();
-require_once('../config.php');
-require_once('../interfaces/constants.php');
-require_once('../interfaces/messages.php');
-require_once('../interfaces/orderErrors.php');
-require_once('../interfaces/productErrors.php');
-require_once('../interfaces/productsVals.php');
-require_once('../interfaces/userErrors.php');
-require_once('../interfaces/emailmanagerErrors.php');
-require_once('../exceptions/notsetted.php');
-//require_once('../interfaces/mysqlVals.php');
+
 require_once("../vendor/autoload.php");
-require_once('../traits/error.php');
-require_once('../traits/emailmanager.trait.php');
-require_once('../traits/sql.trait.php');
-require_once('../traits/ordine.trait.php');
-require_once('../traits/prodotto.trait.php');
-require_once('../traits/utente.trait.php');
-require_once('../objects/emailmanager.php');
-require_once('../objects/utente.php');
-require_once('../objects/prodotto.php');
-require_once('../objects/ordine.php');
-require_once('../objects/carrello.php');
-require_once('paypalConfig.php');
-require_once('config.php');
-require_once('const.php');
 
 use Dotenv\Dotenv;
 use EcommerceTest\Interfaces\UserErrors as Ue;
