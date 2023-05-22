d6ed5debc2c3abc408f89becd32c0a4347fa5ce3
stepp <stepp@mail.com>
2023-05-22 20:34:34 +0200

    Added pages/cart.php

---
+++ pages/cart.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace EcommerceTest\Pages;
+use EcommerceTest\Pages\Partials\Footer;
+use EcommerceTest\Pages\Partials\NavbarLogged;
+
+/**
+ * Cart page
+ */
+class Cart{
+    public static function content(array $params): string{
+        $html = <<<HTML
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Carrello</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href=<?php echo P::REL_CART_CSS; ?>>
+        <link rel="stylesheet" href=<?php echo P::REL_BOOTSTRAP_CSS; ?>>
+        <link rel="stylesheet" href=<?php echo P::REL_JQUERY_CSS; ?> >
+        <link rel="stylesheet" href=<?php echo P::REL_JQUERYTHEME_CSS; ?> >
+        <link rel="stylesheet" href=<?php echo P::REL_FOOTER_CSS; ?> >
+        <script src=<?php echo P::REL_JQUERY_JS; ?>></script>
+        <script src=<?php echo P::REL_JQUERYUI_JS; ?>></script>
+        <script src=<?php echo P::REL_POPPER_JS; ?>></script>
+        <script src=<?php echo P::REL_BOOTSTRAP_JS; ?>></script>
+        <script src=<?php echo P::REL_FOOTER_JS; ?>></script>
+        <script type="module" src=<?php echo P::REL_DIALOG_MESSAGE_JS; ?>></script>
+        <script type="module" src="<?php echo P::REL_LOGOUT_JS; ?>"></script>
+        <script type="module" src=<?php echo P::REL_CART_JS; ?>></script>
+        <script src="https://www.paypalobjects.com/api/checkout.js"></script>
+HTML;
+        if(file_exists('partials/privacy.php') && is_file('partials/privacy.php')){
+            $html .= call_user_func('cookieBanner');
+        }
+        $html .= <<<HTML
+    </head>
+    <body>
+HTML;
+        $html .= NavbarLogged::content($params);
+        $html .= <<<HTML
+        <div id="ordiniT"></div>
+        HTML;
+        $html .= Footer::content();
+        $html .= <<<HTML
+    </body>
+</html>        
+HTML;
+        return $html;
+    }
+}
+?>
