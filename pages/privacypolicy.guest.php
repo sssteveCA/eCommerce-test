2bf2f4c9503d8738e2df86d1b15526a5d8b32174
stepp <stepp@mail.com>
2023-05-23 21:59:50 +0200

    Added pages/privacypolicy.guest.php

---
+++ pages/privacypolicy.guest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace EcommerceTest\Pages;
+
+use EcommerceTest\Objects\Privacy\PrivacyPolicy;
+use EcommerceTest\Pages\Partials\Footer;
+use EcommerceTest\Pages\Partials\NavbarGuest;
+
+/**
+ * Privacy policy page for guest users
+ */
+class PrivacyPolicyGuest{
+
+    public static function content(array $params): string{
+        $html = <<<HTML
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Cookie Policy</title>
+        <meta charset="utf-8">
+	<link rel="stylesheet" href="{$params['paths']['css']['REL_BOOTSTRAP_CSS']}">
+	<link rel="stylesheet" href="{$params['paths']['css']['REL_FOOTER_CSS']}">
+        <link rel="stylesheet" href="{$params['paths']['css']['REL_JQUERY_CSS']}">
+	<link rel="stylesheet" href="{$params['paths']['css']['REL_JQUERYTHEME_CSS']}">
+	<script src="{$params['paths']['js']['REL_BOOTSTRAP_JS']}"></script>
+        <script src="{$params['paths']['js']['REL_FOOTER_JS']}"></script>
+	<script src="{$params['paths']['js']['REL_JQUERY_JS']}"></script>
+        <script src="{$params['paths']['js']['REL_JQUERYUI_JS']}"></script>
+        <script src="{$params['paths']['js']['REL_POPPER_JS']}"></script>
+HTML;
+        $html .= cookieBanner();
+        $html .= <<<HTML
+	</head>
+	<body>
+HTML;
+        $html .= NavbarGuest::content();
+        $html .= PrivacyPolicy::getDocument();
+        $html .= Footer::content();
+        $html .= <<<HTML
+	</body>
+</html>
+HTML;
+        return $html;
+    }
+}
+?>
