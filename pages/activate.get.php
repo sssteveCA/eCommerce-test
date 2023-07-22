12f09c4266768af435510d86c8caa94c8ebfbd22
stepp <stepp@mail.com>
2023-07-22 23:06:43 +0200

    Added CSS and JS files to activate.get.php and activateform.get.php HTML

--- pages/activate.get.php
+++ pages/activate.get.php
@@ -21,6 +21,15 @@ class ActivateGet{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Attivazione account</title>
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_BOOTSTRAP_CSS']}">
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_JQUERY_CSS']}">
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_JQUERYTHEME_CSS']}">
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_FOOTER_CSS']}">
+    <script src="/{$params['paths']['js']['REL_JQUERY_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_JQUERYUI_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_POPPER_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_BOOTSTRAP_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_FOOTER_JS']}"></script>
 HTML;
         if(file_exists('partials/privacy.php') && is_file('partials/privacy.php')){
             $html .= call_user_func('cookieBanner');
--- pages/activateform.get.php
+++ pages/activateform.get.php
@@ -17,6 +17,17 @@ class ActivateFormGet{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Attivazione account</title>
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_ACTIVATE_FORM_CSS']}">
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_BOOTSTRAP_CSS']}">
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_JQUERY_CSS']}">
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_JQUERYTHEME_CSS']}">
+    <link rel="stylesheet" href="/{$params['paths']['css']['REL_FOOTER_CSS']}">
+    <script src="/{$params['paths']['js']['REL_JQUERY_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_JQUERYUI_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_POPPER_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_BOOTSTRAP_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_FOOTER_JS']}"></script>
+    <script src="/{$params['paths']['js']['REL_ACTIVATE_FORM_JS']}"></script>
 HTML;
         if(file_exists('partials/privacy.php') && is_file('partials/privacy.php')){
             $html .= call_user_func('cookieBanner');
