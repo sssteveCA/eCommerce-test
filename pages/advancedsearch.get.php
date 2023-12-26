b29407dbd6e53b6f654d733142d7c415d921a619
= <=>
2023-12-26 20:44:35 +0000

    Added advancedsearch.get.php

---
+++ pages/advancedsearch.get.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace EcommerceTest\Pages;
+
+/**
+ * Advanced search page
+ */
+class AdvancedSearchGet{
+    
+    public static function content(array $params): array{
+        
+    }
+}
+?>
